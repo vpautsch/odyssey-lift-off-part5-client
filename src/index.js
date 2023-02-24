e158db61a67319437a168ed33674d3ad26d9b186
vpautsch <vpautsch@spins.com>
2023-02-24 15:21:11 -0600

    Add beginning of link to uri for client in index.js file.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "odyssey-lift-off-part5-server-production-8210.up.railway.app", // change to YOUR own production server
+  uri: "https://odyssey-lift-off-part5-server-production-8210.up.railway.app", // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
